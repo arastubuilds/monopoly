8aec5918b540f7cb22530ea0dad2c8d7c1460209
Arastu Vij <arastubuilds@gmail.com>
2026-03-01 13:21:39 +0530

    fixed jwt to fit new vercel and render setup

--- react_client/src/store/authStore.js
+++ react_client/src/store/authStore.js
@@ -3,7 +3,8 @@ import { axiosInstance } from "../lib/axios";
 import toast from "react-hot-toast";
 import { io } from "socket.io-client";
 
-const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:8000" : "/";
+// const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:8000" : "/";
+const BASE_URL = import.meta.env.VITE_SERVER_URL;
 console.log(BASE_URL);
 export const useAuthStore = create((set, get) => ({
     
